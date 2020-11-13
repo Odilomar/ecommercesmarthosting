f22a0b390133c5c82b761aa38fcf55928accd451
odilomarjunior.contact@gmail.com <odilomar.junior@gmail.com>
2020-11-12 21:04:44 -0400

    scope(hosting): hosting build

---
+++ precache-manifest.e257eacd57c705a949b634501684f807.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c41a700958b3ed84884fe6c8ffdcbd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c653d627303525f83707",
+    "url": "/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "e6f60d42eb88b58de5ad",
+    "url": "/static/css/main.4f68aa5b.chunk.css"
+  },
+  {
+    "revision": "c653d627303525f83707",
+    "url": "/static/js/2.d4632023.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/static/js/2.d4632023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6f60d42eb88b58de5ad",
+    "url": "/static/js/main.32b7514a.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  }
+]);
